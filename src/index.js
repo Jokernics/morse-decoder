dbcb36cf59cf45d60f9378f61f196d589af89ba7
egor <spirin.egor13@gmail.com>
2022-01-29 00:21:07 +0300

    Doone

--- src/index.js
+++ src/index.js
@@ -38,7 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    sumWords = ''
+    for (let i = 0; i < expr.length; i += 10) {
+        
+        let curentWord = expr.slice(i, i+10)
+        if (curentWord === '**********') {
+            sumWords += ' '
+            continue
+        }
+
+        let wordWithoutZero 
+
+        for (let j = 0; j < curentWord.length; j++) {
+            if (curentWord[j] !== '0') {
+                wordWithoutZero = curentWord.slice(j) 
+                break
+            }
+        }
+        
+        const glosary = {'10': '.', '11': '-'}
+        let wordToMorseDecoding = ''
+        for (let f = 0; f < wordWithoutZero.length; f += 2) {
+            wordToMorseDecoding += glosary[wordWithoutZero.slice(f, f+2)]
+        }
+
+        sumWords += MORSE_TABLE[wordToMorseDecoding]
+        
+    }
+    return sumWords
 }
 
 module.exports = {
